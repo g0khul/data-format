7e924dc706168e82d0e847a95283811a1c5768d1
gokhul <gokhul.tg@galvanizeme.com>
2024-08-19 22:38:05 +0530

    custom, nested, conditional pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ConvertPipe } from './convert.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, ConvertPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'data-format';
+  name = '';
+  date = '';
+  amount = 0;
+  miles = 0;
+
+  onNameChange(event: Event) {
+    const receivedName = event.target as HTMLInputElement;
+    this.name = receivedName.value;
+  }
+
+  onDateChange(event: Event) {
+    const receivedDate = event.target as HTMLInputElement;
+    this.date = receivedDate.value;
+  }
+
+  onAmountChange(event: Event) {
+    const receivedAmount = event.target as HTMLInputElement;
+    this.amount = parseInt(receivedAmount.value);
+  }
+
+  onMilesChange(event: Event) {
+    const receivedMiles = event.target as HTMLInputElement;
+    this.miles = parseInt(receivedMiles.value);
+  }
 }
---
+++ src/app/convert.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'convert',
+  standalone: true,
+})
+export class ConvertPipe implements PipeTransform {
+  transform(value: any, targetUnits: string): any {
+    switch (targetUnits) {
+      case '':
+        return '';
+      case 'km':
+        return value * 1.60934;
+      case 'm':
+        return value * 1.60934 * 1000;
+      case 'cm':
+        return value * 1.60934 * 1000 * 1000;
+      default:
+        throw new Error('Convertion not supported');
+    }
+  }
+}
